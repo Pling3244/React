88a6a58f466eb4539b7fef4b3abf9223435791b6
admin <admin@DESKTOP-5P4GNJM>
2023-04-10 15:31:56 +0900

    test

--- project4/src/main/java/com/example/controller/HomeController.java
+++ project4/src/main/java/com/example/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.example.controller;
 
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		log.info("Welcome home! The client locale is {}." + locale);
+		logger.info("Welcome home! The client locale is {}." + locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat
